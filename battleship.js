6827f241d823a5d24215bfbed304307f3ef5d231
aaronekdahl <aaronekahl@me.com>
2017-01-29 08:26:21 -0800

    renamed-files-input-base-code

---
+++ battleship.js
@@ -0,0 +1,10 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+/*The variable guess has no value until the user had made a guess, it is currently undefined*/
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
